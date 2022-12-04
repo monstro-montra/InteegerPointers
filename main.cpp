8376463b9723fb12bcd6a32d17ca1db362277257
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-12-04 04:32:23 -0500

    this program gets user input for integers and stores them in an array. it prints the array and then tells you the address of each element using dynamic pointers.

--- main.cpp
+++ main.cpp
@@ -40,6 +40,5 @@ int main() {
     intArray::printArray(inputs.arr); //call printArray function on inputs.arr
     intArray::getAddresses();
     system("pause>0");
-    cout << "test";
     return 0;
 }
